fc2c90a8bb95e7322770463dee6b7cf7754b24f3
Angelldca <angeluisdelcollado@gmail.com>
2024-05-10 09:21:24 -0400

    traza rango fecha

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MenuComponent } from '../menu/menu.component';
+
 import { PanelContenidoComponent } from '../panel-contenido/panel-contenido.component';
 import { RouterOutlet } from '@angular/router';
 import { EncabezadoComponent } from '../encabezado/encabezado.component';
@@ -9,7 +9,7 @@ import { MenuPrincipalComponent } from '../menu-principal/menu-principal.compone
 @Component({
   selector: 'app-containerCD',
   standalone: true,
-  imports: [MenuComponent,
+  imports: [
     PanelContenidoComponent,
     RouterOutlet,EncabezadoComponent,
     PiePaginaComponent, MenuPrincipalComponent],
