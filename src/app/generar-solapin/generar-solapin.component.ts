e32511c6337297eaab805949ae7b87b5be183092
AlainRichell <alainrichell@gmail.com>
2024-06-09 23:26:59 -0400

    Generar solapin full

--- src/app/generar-solapin/generar-solapin.component.ts
+++ src/app/generar-solapin/generar-solapin.component.ts
@@ -27,6 +27,7 @@ export class GenerarSolapinComponent implements OnInit {
   tiposSolapin: any[] = [];
   idciudadano: number;
   fecha: string = "";
+  prefijoNumeroSolapin: string = "";
 
   constructor(
     private fb: FormBuilder,
@@ -46,19 +47,71 @@ export class GenerarSolapinComponent implements OnInit {
 
   ngOnInit(): void {
     this.solapinService.getTiposSolapin().subscribe(data => {
-      this.tiposSolapin = data.results;
+      this.tiposSolapin = data;
     });
 
+    if ( String(this.data.idCiudadano.area).match("XETID") ){
+      this.prefijoNumeroSolapin = "X";
+    }else{
+      if ( String(this.data.idCiudadano.roluniversitario).match("Trabajador") ){
+        this.prefijoNumeroSolapin = "T";
+      }
+      if ( String(this.data.idCiudadano.roluniversitario).match("Estudiante") ){
+        this.prefijoNumeroSolapin = "E";
+      }
+      if ( String(this.data.idCiudadano.roluniversitario).match("Visitante") ){
+        this.prefijoNumeroSolapin = "V";
+      }
+      if ( String(this.data.idCiudadano.roluniversitario).match("Familiar") ){
+        this.prefijoNumeroSolapin = "F";
+      }
+    }
+
     this.solapinService.getUltimoNumerosolapin().subscribe(data => {
-      this.solapinForm.patchValue({ numerosolapin: data.numerosolapin });
+      var numerosolapin: string = data.numerosolapin;
+      var numerosolapinNum: number = parseInt(numerosolapin.slice(1));
+      numerosolapinNum++;
+      numerosolapin = this.prefijoNumeroSolapin + String(numerosolapinNum);
+
+      this.solapinForm.patchValue({ numerosolapin: numerosolapin });
     });
 
     this.solapinService.getUltimoCodigobarra().subscribe(data => {
-      this.solapinForm.patchValue({ codigobarra: data.codigobarra });
+      var codigobarra:string = data.codigobarra;
+      var codigobarraNum:number = parseInt(codigobarra.slice(1));
+      codigobarraNum++;
+      codigobarra = this.prefijoNumeroSolapin + String(codigobarraNum);
+
+      this.solapinForm.patchValue({ codigobarra: codigobarra });
     });
 
     this.solapinService.getUltimoSerial().subscribe(data => {
-      this.solapinForm.patchValue({ serial: data.serial });
+      var serial:number = data.serial;
+      serial++;
+      var serialS: string = "";
+      
+      switch ( (serial.toString()).length ){
+        case 6:
+          serialS = serial.toString();
+          break;
+        case 5:
+          serialS = "0" + serial.toString();
+          break;
+        case 4:
+          serialS = "00" + serial.toString();
+          break;
+        case 3:
+          serialS = "000" + serial.toString();
+          break;
+        case 2:
+          serialS = "0000" + serial.toString();
+          break;
+        case 1:
+          serialS = "00000" + serial.toString();
+          break;
+      }
+
+      this.solapinForm.patchValue({ serial: serialS });
     });
   }
 
